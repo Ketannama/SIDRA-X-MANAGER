fe91add0f34b802c370d1959a3bc20d116ba88b9
Sanatani <165205149+Ketannama@users.noreply.github.com>
2024-05-09 13:55:14 +0530

    Update __main__.py

--- ANNIEMUSIC/__main__.py
+++ ANNIEMUSIC/__main__.py
@@ -40,7 +40,7 @@ async def init():
     await userbot.start()
     await JARVIS.start()
     try:
-        await JARVIS.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
+        await JARVIS.stream_call("https://telegra.ph/file/02ad94b1dcc89f0935d78.jpg")
     except NoActiveGroupCall:
         LOGGER("ANNIEMUSIC").error(
             "𝗣𝗹𝗭 𝗦𝗧𝗔𝗥𝗧 𝗬𝗢𝗨𝗥 𝗟𝗢𝗚 𝗚𝗥𝗢𝗨𝗣 𝗩𝗢𝗜𝗖𝗘𝗖𝗛𝗔𝗧\𝗖𝗛𝗔𝗡𝗡𝗘𝗟\n\n˹𝕆𝕩𝕪𝕘𝕖𝕟 ꭙ 𝔹𝕆𝕋˼ 🫧� 𝗕𝗢𝗧 𝗦𝗧𝗢𝗣........"
@@ -50,12 +50,12 @@ async def init():
         pass
     await JARVIS.decorators()
     LOGGER("ANNIEMUSIC").info(
-        "╔═════ஜ۩۞۩ஜ════╗\n  ☠︎︎𝗠𝗔𝗗𝗘 𝗕𝗬 𝕆𝕩𝕪𝕘𝕖𝕟☠︎︎\n╚═════ஜ۩۞۩ஜ════╝"
+        "╔═════ஜ۩۞۩ஜ════╗\n  ☠︎︎𝗠𝗔𝗗𝗘 𝗕𝗬 sanatani☠︎︎\n╚═════ஜ۩۞۩ஜ════╝"
     )
     await idle()
     await app.stop()
     await userbot.stop()
-    LOGGER("ANNIEMUSIC").info("𝗦𝗧𝗢𝗣 ˹𝕆𝕩𝕪𝕘𝕖𝕟 ꭙ 𝔹𝕆𝕋˼ 🫧 𝗠𝗨𝗦𝗜𝗖🎻 𝗕𝗢𝗧..")
+    LOGGER("ANNIEMUSIC").info("𝗦𝗧𝗢𝗣 ˹𝕕𝕒𝕣𝕜 𝕩 𝕞𝕦𝕤𝕚𝕔˼ 🫧 𝗠𝗨𝗦𝗜𝗖🎻 𝗕𝗢𝗧..")
 
 
 if __name__ == "__main__":
